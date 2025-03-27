f9d7d569f99e2700f1e6dfc9bda0be364d5225b3
sebastien-liveyupeng <sebastien.live-yu-peng@laplateforme.io>
2025-03-27 14:24:06 +0100

    Header responsive / Font-Judson / rajouter une transition au pop-up

--- js/accueil.js
+++ js/accueil.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         <span class="text-xs text-gray-500">Temps :</span>
                         <span class="text-gray-700">${recipe.temps_preparation}</span>
                     </div>
-                    <button class="bg-orange-300 text-white px-4 py-2 rounded-md font-bold hover:bg-orange-600 transition-colors open-modal"
+                    <button class="bg-orange-300 text-white px-4 py-2 rounded-md font-bold hover:bg-orange-600 transition-colors open-modal "
                         data-title="${recipe.nom}"
                         data-ingredients="${formatIngredients(recipe.ingredients)}"
                         data-instructions="${formatSteps(recipe.etapes)}"
